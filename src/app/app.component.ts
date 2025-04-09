896919801eb9ecff4b6a837d03804ab79720ef1c
Rubén <rubensedeno@gmail.com>
2025-04-09 18:43:58 +0200

    adds assets and styles.scss

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,9 +4,24 @@ import { RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  template: `
+    <div class="app">
+      <div class="app__header">
+        <img src="/assets/img/logo.svg" class="app__logo" />
+      </div>
+      <div class="app__content">
+        <div class="app__nav">
+          <a routerLink="products" routerLinkActive="active">Products</a>
+        </div>
+        <div class="app__container">
+          <router-outlet />
+        </div>
+        <div class="app__footer">
+          <p>&copy; Ultimate Pizza Inc.</p>
+        </div>
+      </div>
+    </div>
+  `,
+  styleUrl: './app.component.scss',
 })
-export class AppComponent {
-  title = 'ngrx-pizza-app';
-}
+export class AppComponent {}
