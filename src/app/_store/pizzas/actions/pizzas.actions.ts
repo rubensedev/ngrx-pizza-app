84d153ead5d71ab20e1f131962caf49c5133120a
Rubén <rubensedeno@gmail.com>
2025-04-14 12:08:07 +0200

    adds pizzas.actions

---
+++ src/app/_store/pizzas/actions/pizzas.actions.ts
@@ -0,0 +1,14 @@
+import { createAction, props } from '@ngrx/store';
+import { Pizza } from '../../../_interfaces/pizza.interface';
+
+export const loadPizzas = createAction('[Pizzas] Load Pizzas');
+
+export const loadPizzasSuccess = createAction(
+  '[Pizzas] Load Pizzas Success',
+  props<{ payload: Pizza[] }>()
+);
+
+export const loadPizzasFail = createAction(
+  '[Pizzas] Load Pizzas Fail',
+  props<{ payload: any }>()
+);
