b5d1dde0dd46aa2a64b8dfc565af7d18d0bce15f
Rubén <rubensedeno@gmail.com>
2025-04-25 14:05:55 +0200

    router actions and effects

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -9,8 +9,10 @@ import { provideRouter } from '@angular/router';
 // ngrx - store
 import { provideStore } from '@ngrx/store';
 import { provideRouterStore, routerReducer } from '@ngrx/router-store';
+import { provideEffects } from '@ngrx/effects';
 import { provideStoreDevtools } from '@ngrx/store-devtools';
 import { CustomRouteSerializer } from './_store/router/custom-serializer/router.serializer';
+import * as RouterEffects from './_store/router/effects/router.effects';
 
 import { ROUTES } from './app.routes';
 
@@ -20,6 +22,7 @@ export const appConfig: ApplicationConfig = {
     provideRouter(ROUTES),
     provideStore({ router: routerReducer }),
     provideRouterStore({ serializer: CustomRouteSerializer }),
+    provideEffects(RouterEffects),
     provideStoreDevtools({ maxAge: 25, logOnly: !isDevMode() }),
     provideAnimations(),
   ],
